1a2c4266d28e54433cee820bcdd2654ad2cfe190
eduardorcury <eduardoribeirocury@gmail.com>
2020-04-16 15:19:58 -0400

    Endpoint de clientes/id disponível

--- src/main/java/com/eduardorib/cursomc/services/ClienteService.java
+++ src/main/java/com/eduardorib/cursomc/services/ClienteService.java
@@ -6,19 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eduardorib.cursomc.domain.Categoria;
-import com.eduardorib.cursomc.repositories.CategoriaRepository;
+import com.eduardorib.cursomc.domain.Cliente;
+import com.eduardorib.cursomc.repositories.ClienteRepository;
 import com.eduardorib.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class CategoriaService {
+public class ClienteService {
 	
 	@Autowired
-	private CategoriaRepository repo;
+	private ClienteRepository repo;
 	
-	public Categoria find(Integer id) {
+	public Cliente find(Integer id) {
 		
-		Optional<Categoria> obj = repo.findById(id);
+		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 }
