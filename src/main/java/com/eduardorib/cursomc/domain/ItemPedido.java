69b3a194e46a618179381e1a617e0e445b483da3
eduardorcury <eduardoribeirocury@gmail.com>
2020-04-18 17:21:58 -0400

    Endpoint /pedidos/id

--- src/main/java/com/eduardorib/cursomc/domain/ItemPedido.java
+++ src/main/java/com/eduardorib/cursomc/domain/ItemPedido.java
@@ -5,11 +5,14 @@ import java.io.Serializable;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class ItemPedido implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
-
+	
+	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
 	
@@ -28,6 +31,7 @@ public class ItemPedido implements Serializable{
 		this.preco = preco;
 	}
 	
+	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
@@ -44,12 +48,12 @@ public class ItemPedido implements Serializable{
 		this.id = id;
 	}
 
-	public Double getDesconte() {
+	public Double getDesconto() {
 		return desconto;
 	}
 
-	public void setDesconte(Double desconte) {
-		this.desconto = desconte;
+	public void setDesconto(Double desconto) {
+		this.desconto = desconto;
 	}
 
 	public Integer getQuantidade() {
--- src/main/java/com/eduardorib/cursomc/services/PedidoService.java
+++ src/main/java/com/eduardorib/cursomc/services/PedidoService.java
@@ -5,20 +5,20 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.eduardorib.cursomc.domain.Cliente;
-import com.eduardorib.cursomc.repositories.ClienteRepository;
+import com.eduardorib.cursomc.domain.Pedido;
+import com.eduardorib.cursomc.repositories.PedidoRepository;
 import com.eduardorib.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
-public class ClienteService {
+public class PedidoService {
 
 	@Autowired
-	private ClienteRepository repo;
+	private PedidoRepository repo;
 
-	public Cliente find(Integer id) {
+	public Pedido find(Integer id) {
 
-		Optional<Cliente> obj = repo.findById(id);
+		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 }
