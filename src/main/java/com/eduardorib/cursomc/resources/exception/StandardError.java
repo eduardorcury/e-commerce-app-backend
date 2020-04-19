4bd495e5831803afbfdc32a8bbbb33783a677d81
eduardorcury <eduardoribeirocury@gmail.com>
2020-04-19 16:01:46 -0400

    Metódo DELETE e tratamento de exceção

--- src/main/java/com/eduardorib/cursomc/resources/exception/StandardError.java
+++ src/main/java/com/eduardorib/cursomc/resources/exception/StandardError.java
@@ -40,7 +40,5 @@ public class StandardError implements Serializable {
 	public void setTimeStamp(Long timeStamp) {
 		this.timeStamp = timeStamp;
 	}
-	
-	
 
 }
--- src/main/java/com/eduardorib/cursomc/services/CategoriaService.java
+++ src/main/java/com/eduardorib/cursomc/services/CategoriaService.java
@@ -3,6 +3,7 @@ package com.eduardorib.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.eduardorib.cursomc.domain.Categoria;
@@ -26,13 +27,33 @@ public class CategoriaService {
 		
 		obj.setId(null);
 		return repo.save(obj);
-		
 	}
 	
 	public Categoria update(Categoria obj) {
 		
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
 		
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException e){
+			
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
