9ce44f48b7c12e9e702ec0425a3860f8208b96fb
eduardorcury <eduardoribeirocury@gmail.com>
2020-04-27 13:47:54 -0400

    Inserindo Pedido

--- src/main/java/com/eduardorib/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/eduardorib/cursomc/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.eduardorib.cursomc.domain;
 import javax.persistence.Entity;
 
 import com.eduardorib.cursomc.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento{
 	
 	private static final long serialVersionUID = 1L;
