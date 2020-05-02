d2cae74f598eb4d81c7fdb3a6c5559ad3cf0f915
eduardorcury <eduardoribeirocury@gmail.com>
2020-05-02 17:02:40 -0400

    Email HTML

--- src/main/java/com/eduardorib/cursomc/services/EmailService.java
+++ src/main/java/com/eduardorib/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.eduardorib.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.eduardorib.cursomc.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido pedido);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 
 }
