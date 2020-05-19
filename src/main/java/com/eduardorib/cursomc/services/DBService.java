1e8da03d3e71497171794e605e4ba892266e54a2
eduardorcury <eduardoribeirocury@gmail.com>
2020-05-19 18:37:05 -0400

    Aumentando o tamanho máximo permitido para upload

--- src/main/java/com/eduardorib/cursomc/services/DBService.java
+++ src/main/java/com/eduardorib/cursomc/services/DBService.java
@@ -186,7 +186,7 @@ public class DBService {
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11));
 		produtoRepository.saveAll(Arrays.asList(p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25,
-				p26, p27, p28, p29, p30, p31, p32, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47,
+				p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40, p41, p42, p43, p44, p45, p46, p47,
 				p48, p49, p50));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
